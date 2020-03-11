0892765f4419e7c49ebce0fc490c91c544a90bf4
jfmatheusg <jfmatheusg@gmail.com>
2020-03-10 22:43:30 -0500

    Paso3-Se modifica codigo para que sume los dos numeros de entrada(Verde)

--- Calculadora.py
+++ Calculadora.py
@@ -2,6 +2,8 @@ class Calculadora:
     def sumar(cadena):
         if cadena =="":
             return 0
+        elif "," in cadena:
+            return int(cadena[0])+int(cadena[2])
         else:
             return int(cadena)
 
