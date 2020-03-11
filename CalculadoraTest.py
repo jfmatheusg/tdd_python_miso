6ab4773070d786e4c685925316e7dee7d68a874a
jfmatheusg <jfmatheusg@gmail.com>
2020-03-10 22:45:28 -0500

    Paso4-Se agrega prueba para varios numeros de entrada(Rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -15,3 +15,6 @@ class CalculadoraTest(TestCase):
 
     def test_sumar_cadenacondosNumeros(self):
         self.assertEqual(Calculadora.sumar(("1,3")), 4, "Dos numeros")
+
+    def test_sumar_cadenaconvariosNumeros(self):
+        self.assertEqual(Calculadora.sumar(("5,2,4,1")), 12, "Varios numeros")
